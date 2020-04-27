eb54d4398ee4b8ff62671e2369ad0a5c16f91e89
akshatb19 <akshat@amagi.com>
2020-04-27 15:45:26 +0530

    Commit Feature Spec

--- spec/features/uploads_spec.rb
+++ spec/features/uploads_spec.rb
@@ -7,32 +7,34 @@ RSpec.feature "Uploads", type: :feature do
 
   scenario "upload file to all uploads" do
     visit new_upload_path
-    find("All Uploads").click
+    find('a[href$="/uploads"]').click
     expect(page.current_path).to eql(uploads_path)
   end
 
   scenario "all uploads to upload file" do
     visit uploads_path
-    find("New Upload").click
+    find('a[href$="/uploads/new"]').click
     expect(page.current_path).to eql(new_upload_path)
   end
 
   scenario "Upload file successfully" do
     visit new_upload_path
-    find("Choose File").click
-    attach_file("file", Rails.root + 'spec/fixtures/abc.text')
-    find("Upload file").click
+    find("#file").click
+    attach_file("file", Rails.root + 'spec/fixtures/abc.txt')
+    find("input[type = 'submit']").click
     expect(page.current_path).to eql(uploads_path)
   end
 
   scenario "view uploaded file" do
     visit uploads_path
-    
+    find('a',text: "abc.txt").click
+    expect(page.current_path).to eql("/abc.txt")
   end
 
   scenario "delete file" do
     visit uploads_path
-
+    find('a[href$="/uploads/62"]').click
+    expect(page).to_not have_css('a',text: "abc.txt")
   end
 
 end
