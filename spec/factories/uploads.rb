6a0919758844f4a319181cc059f02766e724270b
akshatb19 <akshat@amagi.com>
2020-04-28 16:08:41 +0530

    Commit changes

--- spec/factories/uploads.rb
+++ spec/factories/uploads.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :upload do
+    id {63}
     name {"Test_name"}
     url {"Test_url"}
   end
--- spec/models/upload_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Upload, type: :model do
-  
-end
